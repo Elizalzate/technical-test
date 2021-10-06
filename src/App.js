e2fae50e7ee1d0254445ec493f265d66edf0735f
Elizabeth Alzate <67381475+Elizalzate@users.noreply.github.com>
2021-10-05 23:49:35 -0500

    Se importa Dashboard en App.js, se organizan los imports en Dashboard

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import './App.css';
-import { Tarea } from './Components/Tarea';
-import { ListHeader } from './Components/ListHeader';
+import { Dashboard } from './Components/Dashboard';
 
-const tareas = [
- { id:1, nombre: "Buy new sweatshirt", completado:true},
- { id:2, nombre: "Read an article", completado:false},
- { id:3, nombre: "Watch a movie", completado:false},
- { id:4, nombre: "Listen to a podcast", completado:false},
-];
-
-const agregarTarea = ()=>{
-
-  let id = tareas.length + 1;
-  let nombre = document.getElementById('nuevaTarea').value;
-  tareas.push({ id: id, nombre: nombre, completado: false});
-
-};
 function App() {
   return (
     <div className="App">
-      <ListHeader/>
-      {
-        tareas.map( 
-          tarea => <Tarea tarea={tarea}/>
-        )
-      }
-      <input id="nuevaTarea" type="text" placeholder="nueva tarea"/>
-      <button onClick={agregarTarea}></button>
+      <Dashboard/>
     </div>
   );
 }
