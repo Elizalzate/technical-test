b67ae58cc1eedfa6ffa3277fc4ef75d7686e6a93
Elizabeth Alzate <67381475+Elizalzate@users.noreply.github.com>
2021-10-05 23:48:39 -0500

    Se pasa todo lo que estaba en App.js a Dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,1 +1,34 @@
 import React, { useState } from 'react';
+import { Tarea } from './Tarea';
+import { ListHeader } from './ListHeader';
+
+export const Dashboard = () => {
+    const [tareas, setTareas] = useState([
+        { id:1, nombre: "Buy new sweatshirt", completado:true},
+        { id:2, nombre: "Read an article", completado:false},
+        { id:3, nombre: "Watch a movie", completado:false},
+        { id:4, nombre: "Listen to a podcast", completado:false},
+       ]);
+
+    const agregarTarea = ()=>{
+
+    let id = tareas.length + 1;
+    let nombre = document.getElementById('nuevaTarea').value;
+    tareas.push({ id: id, nombre: nombre, completado: false});
+    
+    };
+
+    return(
+        <>
+            <ListHeader/>
+            {
+                tareas.map( 
+                tarea => <Tarea tarea={tarea}/>
+                )
+            }
+            <input id="nuevaTarea" type="text" placeholder="nueva tarea"/>
+            <button onClick={agregarTarea}></button>
+        </>
+    );
+
+};
